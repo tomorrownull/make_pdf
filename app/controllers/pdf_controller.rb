e2b90bc19dbb91bbd55fe9a76ad97bf61a968fb1
ming <ming@diningcity.aisa>
2016-04-20 12:04:30 +0800

    add file to cache

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -1,9 +1,12 @@
 class PdfController < ApplicationController
 
   def make
-    pdf = WickedPdf.new.pdf_from_url(params[:url])
     url = URI.parse(params[:url])
     filename = params[:filename] || url.host
+    Rails.cache.remove(url.to_s) if params[:refersh]
+    pdf = Rails.cache.fetch(url.to_s) do
+      WickedPdf.new.pdf_from_url(url.to_s)
+    end
     send_data pdf,filename: "#{filename}.pdf" , :type => "application/pdf"
   end
 end
