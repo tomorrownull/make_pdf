7153e78a7a458d1ebb1ca5ae35c401cf0ea4274b
ming <ming@diningcity.aisa>
2016-04-21 09:52:30 +0800

    发布地址改变 force clear cache

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -3,8 +3,8 @@ class PdfController < ApplicationController
   def make
     url = URI.parse(params[:url])
     filename = params[:filename] || url.host
-    Rails.cache.remove(url.to_s) if params[:refersh]
-    pdf = Rails.cache.fetch(url.to_s) do
+    pdf = Rails.cache.fetch(url.to_s,force: params[:refersh].present?) do
+      Rails.logger.info('make pdf')
       WickedPdf.new.pdf_from_url(url.to_s)
     end
     send_data pdf,filename: "#{filename}.pdf" , :type => "application/pdf"
