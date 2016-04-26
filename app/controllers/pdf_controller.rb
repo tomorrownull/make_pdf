cfbd46bebb4d7c1426d80cbe55a8d6c97c071ee0
ming <ming@diningcity.aisa>
2016-04-26 17:21:48 +0800

    add inline for browser

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -6,6 +6,6 @@ class PdfController < ApplicationController
     pdf = Rails.cache.fetch(url.to_s,force: params[:refresh].present?) do
       WickedPdf.new.pdf_from_url(url.to_s)
     end
-    send_data pdf,filename: "#{filename}.pdf" , :type => "application/pdf;charset=utf-8; header=present"
+    send_data pdf,filename: "#{filename}.pdf" , :type => "application/pdf;charset=utf-8; header=present", disposition: 'inline'
   end
 end
