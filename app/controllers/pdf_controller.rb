c0ee70a83cbed75e2089200e7a8dec1ab5b5b5c1
ming <ming@diningcity.aisa>
2016-04-27 09:44:43 +0800

    add export to file

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -6,6 +6,10 @@ class PdfController < ApplicationController
     pdf = Rails.cache.fetch(url.to_s,force: params[:refresh].present?) do
       WickedPdf.new.pdf_from_url(url.to_s)
     end
+    save_path = Rails.root.join('public/pdfs',"#{filename}.pdf")
+    File.open(save_path, 'wb') do |file|
+      file << pdf
+    end
     send_data pdf,filename: "#{filename}.pdf" , :type => "application/pdf;charset=utf-8; header=present", disposition: 'inline'
   end
 end
