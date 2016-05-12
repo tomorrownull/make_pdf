bee74711d4f60f24f5da89c6573836f0ba9fa31e
ming <ming@diningcity.aisa>
2016-05-12 17:15:42 +0800

    fix

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -3,7 +3,7 @@ class PdfController < ApplicationController
   def make
     url = URI.parse(get_params[:url])
     filename = get_params[:filename] || url.host
-    MakePdfJob.perform_later(url,filename)
+    MakePdfJob.perform_later(url.to_s,filename)
     render status: 200
   end
 
