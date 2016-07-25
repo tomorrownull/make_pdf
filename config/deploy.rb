4f49d2355f51fc30a10415df5314212b49f419c1
ming <ming@diningcity.aisa>
2016-07-25 14:40:35 +0800

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :nginx_server_name,'generator.restaurantweek.cn'
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system','public/pdfs')
-
+set :delayed_job_args, "-n 2"
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
